fcdf33d8ff99773a1e1e6b9989af5eed36789611
nomisity <s-kobaya@jaist.ac.jp>
2015-02-21 17:10:57 +0900

    t

--- js/setting.js
+++ js/setting.js
@@ -11,8 +11,10 @@ Taxes.capitaBasis = 3500; //均等割 平成25年度
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'nomi_budget_2013';
-OpenSpending.year = '2013';
+//OpenSpending.identifier = 'nomi_budget_2013';
+OpenSpending.identifier = UseData.identifier;
+//OpenSpending.year = '2013';
+OpenSpending.year = UseData.year;
 
 OpenSpending.Styles = OpenSpending.Styles || {};
 
